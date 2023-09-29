b928bf9a80d0514e8602af94a9cf6d664e791c47
Juan Cucalon <juan.cucalon@cwc.com>
2023-09-29 12:26:47 -0500

    settings update

--- alex/settings.py
+++ alex/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%6t0c(r7@lwp6*omv1g4(0wh==k=2f-wjkzkar7448-90ob7hk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
